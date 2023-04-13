a866f1820e02cc43f8f92850ad376eff154ad6a9
gurkanmaral <gurkanmaral1@gmail.com>
2023-04-13 17:11:07 +0300

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,11 @@
+import CustomButton from "./CustomButton";
+import ColorPicker from "./ColorPicker"
+import FilePicker from "./FilePicker"
+import AIPicker from "./AIPicker"
+import Tab from "./Tab"
+
+export {CustomButton,
+ColorPicker,
+FilePicker,
+AIPicker,
+Tab}
